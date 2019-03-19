a6066e6c05fa9bd8d09c57be24700b45457f0dec
kalasikk <kalasikk@gmail.com>
2019-03-19 10:52:15 +0300

    add ready function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  
+
+function findPowerOfP(N, p){
+  var count = 0;
+  var r = p;
+  while (r <= N){  
+  count += Math.floor(N / r); 
+  r = r * p ;
+  }
+  return count ;
+    }
+  
+  function primeFactorsofB(B){ 
+    
+    
+     var ans=[]; 
+    
+      for (let i = 2;; i++) {
+        if(B==1) break;
+          if (B % i == 0) { 
+              var count = 0; 
+              while (B % i == 0) { 
+                  B = Math.floor(B / i); 
+                  count++; 
+              } 
+    
+              ans.push([i, count]); 
+          } 
+      } 
+      return ans; 
+  } 
+   
+  function largestPowerOfB(N, B){ 
+  var vec = [] ;
+  vec = primeFactorsofB(B) ;
+  var ans1 ;
+  var ansvect=[];
+  for(let i=0;i<vec.length;i++){
+    ansvect.push(Math.floor(findPowerOfP(N,vec[i][0])/vec[i][1]));
+}
+  
+    ans1 = Math.min(...ansvect);
+  return ans1; 
+  }
+    return largestPowerOfB(number,base); 
 }
